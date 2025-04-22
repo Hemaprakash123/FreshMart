4731744e52cae3e7423160cf986c390fcf35574a
Hema prakash <hemaprakshreddy73@gmail.com>
2025-04-22 20:35:16 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -41,7 +41,33 @@ import orderRoutes from './routes/orderRoutes.js';
 
 
 // Connect to MongoDB
-import '../database/db.js';
+// import '../database/db.js';
+
+
+const mongoose = require('mongoose');
+const mongourl = 'mongodb+srv://hemaprakash:hemaprakash123$@cluster0.dp4do.mongodb.net/ecommerce';
+
+mongoose.connect(mongourl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('connected', () => {
+  console.log("MongoDB connection successful");
+});
+
+db.on('error', (err) => {
+  console.log("MongoDB connection error: ", err);
+});
+
+db.on('disconnected', () => {
+  console.log("MongoDB connection disconnected");
+});
+
+module.exports = mongoose;
+
 
 // Initialize app
 const app = express();
